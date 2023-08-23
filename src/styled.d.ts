9b35fa86d26034c6f7dc23f0bd8bb231880f3844
JHKIMS <wjdgns5131@gmail.com>
2023-08-23 20:01:23 +0900

    Updates

--- src/styled.d.ts
+++
@@ -1,9 +0,0 @@
-import "styled-components";
-
-declare module "styled-components"{
-    export interface DefaultTheme{
-        cardColor: string;
-        bgColor: string;
-        boardColor: string;
-    }
-}
--- src/theme.ts
+++
@@ -1,9 +0,0 @@
-import { DefaultTheme } from "styled-components";
-
-export const darkTheme: DefaultTheme = {
-  bgColor: "#f1f2f6",
-  cardColor:"white",
-  boardColor:"#3397D7",
-};
-
-
